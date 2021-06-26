ba65e376c78d99fe54c25af70759cc3042637f98
Brandon6134 <brandonkong@rogers.com>
2021-06-26 15:27:01 -0400

    added accordian drop down tabs for food categories under Get Started

--- react-application/src/components/Start.js
+++ react-application/src/components/Start.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Button from 'react-bootstrap/Button';
-import FormControl from 'react-bootstrap/FormControl';
+//import InputGroup from 'react-bootstrap/InputGroup';
+//import Button from 'react-bootstrap/Button';
+//import FormControl from 'react-bootstrap/FormControl';
+import { Accordion, Card, Button, FormControl, InputGroup } from "react-bootstrap";
+
+const tabs = [
+  { id: 1, label: "Produce", description: "*insert food stuff here" },
+  { id: 2, label: "Bakery", description: "Content of Tab 2" },
+  { id: 3, label: "Deli", description: "Content of Tab 3" },
+  { id: 4, label: "Frozen Meat", description: "Content of Tab 2" },
+  { id: 5, label: "Organic Materials", description: "Content of Tab 2" },
+  { id: 6, label: "Baking Ingredients", description: "Content of Tab 2" },
+  { id: 7, label: "Sauces/Condiments", description: "Content of Tab 2" },
+  { id: 8, label: "Cereals/Snacks", description: "Content of Tab 2" },
+  { id: 2, label: "Drinks", description: "Content of Tab 2" },
+  { id: 2, label: "Dairy", description: "Content of Tab 2" },
+  { id: 2, label: "Frozen Goods", description: "Content of Tab 2" }
+];
 
 function Start() {
     return (
@@ -18,6 +33,22 @@ function Start() {
             <Button variant="outline-secondary">Add</Button>
           </InputGroup.Prepend>
         </InputGroup>
+
+        {tabs.map(tab => (
+        <Accordion>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey={tab.id}>
+                {tab.label}
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey={tab.id}>
+              <Card.Body>{tab.description}</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      ))}
+
       </div>
     );
 }
