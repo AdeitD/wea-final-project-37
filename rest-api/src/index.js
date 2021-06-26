4ce24ceb0fbe4ed913be50d0af5dc073bb018f4c
gsupersa <gsupersa@uwaterloo.ca>
2021-06-26 10:46:25 -0400

    updated index.js in rest-api

---
+++ rest-api/src/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import data from "../data";
+
+
+const app = express();
+const PORT = process.env.PORT ||3000;
+app.use(express.json());
+app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+
+
+import groceries-parameters from './routes/groceries-parameters';
+import groceries from './routes/groceries';
+
+app.use('/api/v1', groceries-parameters);
+app.use('/api/v1', groceries);
+
+
+app.listen(PORT, () =>{
+    console.log(`Server listening on port ${PORT}`);
+});
