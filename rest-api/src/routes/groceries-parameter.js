14caa06939fadc3ea63959605b1f3bfe3a176ebc
gsupersa <gsupersa@uwaterloo.ca>
2021-06-26 10:49:37 -0400

    search by id and category endpoints

--- rest-api/src/routes/groceries-parameter.js
+++ rest-api/src/routes/groceries-parameter.js
@@ -5,10 +5,47 @@ import binSearch from '../bin-search.js';
 
 const gpRouter = express.Router();
 
-// TODO: Get grocery by id
+//grocery by id
+gpRouter.get('/groceries/:id', (req, res)=>{              
+    let id = parseInt(req.params.id); 
+    if (isNaN(id)){
+        res.status(404).json({message: `Invalid id`});
+    }
+    else{
+    let response= data.find((grocery) => grocery.id === id);
+    if (!response){
+        res.status(404).json({message: `Grocery with ID: ${id} does not exist`});
+    }
+    res.json(response).status(200)
+}
+});
 
-// TODO: Get grocery by name
+/*
+//grocery by name
+gpRouter.get('/groceries/:name', (req, res)=>{
+    let name = req.params.name;
+    //check for capitalization of first letter
+    let response= data.find((grocery) => grocery.name === name);
+    if (!response){
+        res.status(404).json({message: `Grocery with name: ${name} does not exist`});
+    }
+    res.json(response).status(200)
+}
+});
+*/
 
-// TODO: Get grocery by category
+//grocery by category
+gpRouter.get('/groceries/:category', (req, res)=>{
+    let category = req.params.category;
+    //check for capitalization of categories as well?
+
+    let response= data.filter((grocery) => grocery.category === category);
+    if (!response){
+        res.status(404).json({message: `Grocery category: ${category} does not exist`});
+    }
+    res.json(response).status(200)
+}
+});
+
 
 export default gpRouter;
