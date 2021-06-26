6885984832c0cded9d3fa99a9cdc19e1e1c06648
gsupersa <gsupersa@uwaterloo.ca>
2021-06-26 10:48:13 -0400

    full grocery list and adding a grocery endpoints

--- rest-api/src/routes/groceries.js
+++ rest-api/src/routes/groceries.js
@@ -5,8 +5,28 @@ import data from '../../data.js';
 
 const gRouter = express.Router();
 
-// TODO: Get list of all groceries
+//entire grocery list
+gRouter.get('/api/v1/groceries', (req, res)=>{
+    res.json(data).status(200);
+});
 
-// TODO: Add a grocery to the API and update the data.js file
+//adds new grocery to list
+gRouter.post("/api/v1/groceries", (request, response) => {
+    if (!req.body.name) {
+        return res.status(400).json({ error: "Grocery needs a name parameter." });
+        }
+    if (!req.body.category) {
+        return res.status(400).json({ error: "Grocery needs a category parameter." });
+        }
+    const { name, category } = request.body;
+    const NewGrocery= {
+        id: data.length +1
+        name: name;
+        category: category;
+    };
+    data.push(NewGrocery);
+    return response.status(201).json(NewGrocery)
+}
+
 
 export default gRouter;
