0daab14f045104ca79693400520f2a7f8768cfed
Jarod Hillman <jarod@Jarods-MBP.hitronhub.home>
2021-06-26 18:01:07 -0400

    Added error handling for invalid grocery categories, and added checking to ensure the first letter of each grocery is capitalized.

--- rest-api/src/routes/groceries-parameter.js
+++ rest-api/src/routes/groceries-parameter.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import data from '../../data.js';
 import binSearch from '../bin-search.js';
+import categories from '../../categories.js';
 
 const gpRouter = express.Router();
 
@@ -24,6 +25,15 @@ gpRouter.get('/groceries/id/:id', (req, res)=>{
 gpRouter.get('/groceries/name/:name', (req, res)=> {
     let name = req.params.name;
 
+    // Make sure the name has the potential to be valid before conducting search
+    // Check if the first letter of each word is capitalized:
+    let words = name.split(' ');
+    for (let j = 0; j < words.length; ++j) {
+        if (words[j][0] < 'A' || words[j][0] > 'Z') {
+            return res.status(400).json({ error: "All words in the grocery must start with a capital letter." });
+        }
+    }
+
     let response = binSearch(data, name);
     if (!response){
         res.status(404).json({message: `Grocery with name: ${name} does not exist`});
@@ -34,6 +44,18 @@ gpRouter.get('/groceries/name/:name', (req, res)=> {
 //grocery by category
 gpRouter.get('/groceries/category/:category', (req, res)=>{
     let category = req.params.category;
+
+    // Make sure category is a valid category
+    let isValid = false;
+    categories.forEach(cat => {
+        if (category === cat) {
+            isValid = true;
+        }
+    });
+    
+    if (!isValid) {
+        return res.status(400).json({ error: `Provided category: ${category} is invalid` });
+    }
 
     let response = data.filter((grocery) => grocery.category === category);
     if (!response){
