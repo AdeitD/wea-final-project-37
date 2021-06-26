f35bfa644f31be3f3e1afda39a8ce61c53248019
Brandon6134 <brandonkong@rogers.com>
2021-06-26 13:39:13 -0400

    Committed grocery name search bar and add button

--- react-application/src/components/Start.js
+++ react-application/src/components/Start.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
 
 function Start() {
     return (
       <div>
         <h2>Get Started</h2>
+        
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="Enter Grocery Name"
+            aria-label="Enter Grocery Name"
+            aria-describedby="basic-addon2"
+          />
+          <InputGroup.Prepend>
+            <Button variant="outline-secondary">Add</Button>
+          </InputGroup.Prepend>
+        </InputGroup>
       </div>
     );
 }
