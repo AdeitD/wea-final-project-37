f5c75b6e4a1477ac822ffcb67874a080ed7f97ed
AdeitD <adeitdalal@gmail.com>
2021-06-28 15:59:40 -0400

    Added toast warnings for items that were already added from the Browse section.

--- react-application/src/components/BrowseListItem.js
+++ react-application/src/components/BrowseListItem.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './BrowseListItem.css';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function BrowseListItem({ name, onClick }) {
+    const [listItem, setItemState] = useState('0');
 
     const handleClick = () => {
-        toast.success(`Grocery "${name}" Added!`);
         onClick(name);
+
+        if (listItem === '0') {
+            toast.success(`Grocery "${name}" Added!`);
+            setItemState('1');
+        } else {
+            toast.warn(`Grocery "${name}" is already added!`);
+        }
     }
 
     return <div className="list-item" onClick={handleClick}>{name}</div>
