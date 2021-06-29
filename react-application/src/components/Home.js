c17dbeb3eaf3f3b8fceb9b89418f3c096c57eef7
AdeitD <adeitdalal@gmail.com>
2021-06-29 16:50:19 -0400

    Updated Home page information

--- react-application/src/components/Home.js
+++ react-application/src/components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 function Home() {
     return (
         <div>
@@ -10,10 +9,10 @@ function Home() {
                 so you can quickly find all the items you need!</p>
             <p style={{position:"relative", left:"10px"}}>The 'Get Started' page will help you set up a categorized grocery list, whereas 
                 the 'Browse' page will let you search our database for items you might like to add to your list.</p>
+            <p style={{position:"relative", left:"10px"}}>Our database is by no means all-encompasing! If you have a grocery item that isn't
+                in our database, head over to the 'Add' page to add it in.</p>
         </div>
     );
 }
-
-
 
 export default Home;
