d97c6f7bb0a88d5c1b642c59920dcbd650ea2e7d
Brandon6134 <brandonkong@rogers.com>
2021-06-29 15:38:16 -0400

    added text input and add button to Add

--- react-application/src/components/Add.js
+++ react-application/src/components/Add.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Button, FormControl, InputGroup } from "react-bootstrap";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function Add() {
@@ -6,6 +9,20 @@ function Add() {
         <div>
             <h2 style={{position:"relative", left:"10px"}}>Add Grocery Items</h2>
             <p style={{position:"relative", left:"10px"}}>Add new grocery items to our database below!</p>
+            
+            <InputGroup className="mb-3">
+        
+                <InputGroup.Prepend>
+                    <Button variant="primary" >Add</Button>
+                </InputGroup.Prepend>
+                <FormControl
+                placeholder="Enter New Grocery Name"
+                aria-label="Enter New Grocery Name"
+                aria-describedby="basic-addon2"
+                //value={groceryName}
+                //onChange={handleChangeName}
+                />
+            </InputGroup>
         </div>
     );
 }
